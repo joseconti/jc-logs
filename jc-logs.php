437d304875c9a61b5c4f7c1bf80d1a85301f7a89
José Conti <j.conti@joseconti.com>
2024-10-03 15:17:12 +0200

    Update jc-logs.php

--- jc-logs.php
+++ jc-logs.php
@@ -14,20 +14,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+// Require the necessary class files immediately.
+require_once plugin_dir_path( __FILE__ ) . 'includes/Psr/Log/logger-interface.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/Psr/Log/class-loglevel.php';
+require_once plugin_dir_path( __FILE__ ) . 'classes/class-jc-log.php';
+require_once plugin_dir_path( __FILE__ ) . 'classes/class-jc-log-admin.php';
+
+// Initialize the plugin after WordPress has fully loaded.
 add_action( 'plugins_loaded', 'initialize_jc_logs', 1 );
+
 /**
  * Initialize the JC Logs plugin.
  */
 function initialize_jc_logs() {
-
-	require_once plugin_dir_path( __FILE__ ) . 'includes/Psr/Log/logger-interface.php';
-	require_once plugin_dir_path( __FILE__ ) . 'includes/Psr/Log/class-loglevel.php';
-	require_once plugin_dir_path( __FILE__ ) . 'classes/class-jc-log.php';
-	require_once plugin_dir_path( __FILE__ ) . 'classes/class-jc-log-admin.php';
-
 	JC_Logs\JC_Log_Admin::get_instance();
 }
 
 // Activación y desactivación del plugin.
-register_activation_hook( __FILE__, array( 'JC_Logs\JC_Log', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'JC_Logs\JC_Log', 'deactivate' ) );
+register_activation_hook( __FILE__, array( 'JC_Logs\\JC_Log', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'JC_Logs\\JC_Log', 'deactivate' ) );
